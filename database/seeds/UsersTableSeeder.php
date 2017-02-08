8d627a31aa1b806575553f5f0b3c9d070e9afd75
Thanapong Prathumchat <thanapong.pr.56@ubu.ac.th>
2017-02-08 13:44:36 +0700

    Add users table seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //DB::table('users')->truncate();
+        
+        DB::table('users')->insert([
+            'username' => 'admin',
+            'password' => bcrypt(env('ADMIN_DEFAULT_PASS')),
+            'email' => 'admin@admin.com',
+            'title' => 'นาย',
+            'name'     => 'admin',
+            'lastname' => 'admin',
+            'faculty_id' => '1',
+            'type_id' => '1',
+            'active' => 1,
+        ]);
+    }
+}
